5e6b15a3546b3aaaa0d860ef3ecaeeca40d604e1
Kerrian SALAUN <ks@mobaitec.fr>
2024-06-13 11:39:08 +0200

    tools bar end

--- lib/components/pdf_annotations.dart
+++ lib/components/pdf_annotations.dart
@@ -1,142 +1,129 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:mairie_ipad/components/tools_bar.dart';
 
 class AnnotationsPDF extends StatefulWidget {
   final File pdfFile;
 
-  const AnnotationsPDF({Key? key, required this.pdfFile}) : super(key: key);
+  AnnotationsPDF({Key? key, required this.pdfFile}) : super(key: key);
 
   @override
   _AnnotationsPDFState createState() => _AnnotationsPDFState();
 }
 
 class _AnnotationsPDFState extends State<AnnotationsPDF> {
-  late PDFViewController _pdfViewController;
+  late PdfViewerController _pdfViewerController;
+  PdfDocument? _pdfDocument; // Use nullable PdfDocument
   int _totalPages = 0;
   int _currentPage = 0;
   bool _isReady = false;
-  String? _tempFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    _pdfViewerController = PdfViewerController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Annotations PDF'),
+        title: Text('Visualisation du PDF'),
       ),
       body: Stack(
         children: [
-          PDFView(
-            filePath: widget.pdfFile.path,
-            enableSwipe: true,
-            swipeHorizontal: true,
-            autoSpacing: false,
-            pageFling: true,
-            onRender: (pages) {
+          SfPdfViewer.file(
+            widget.pdfFile,
+            controller: _pdfViewerController,
+            onDocumentLoaded: (PdfDocumentLoadedDetails details) {
               setState(() {
-                _totalPages = pages!;
+                _pdfDocument = details.document;
+                _totalPages = _pdfDocument!.pages.count;
                 _isReady = true;
               });
             },
-            onViewCreated: (PDFViewController pdfViewController) {
-              _pdfViewController = pdfViewController;
-            },
-            onPageChanged: (int? page, int? total) {
+            onPageChanged: (PdfPageChangedDetails details) {
               setState(() {
-                _currentPage = page!;
+                _currentPage = details.newPageNumber;
               });
             },
           ),
-          if (!_isReady)
-            Center(
-              child: CircularProgressIndicator(),
+          Positioned(
+            top: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              color: Colors.blue.shade900,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ToolbarPDF(
+                    onAnnotationSelected: (type) {
+                      // Handle annotation selection here
+                      switch (type) {
+                        case AnnotationType.Pen:
+                          // Logic for pen annotation
+                          break;
+                        case AnnotationType.Text:
+                          // Logic for text annotation
+                          break;
+                        case AnnotationType.Rectangle:
+                          _addRectangleAnnotation();
+                          break;
+                        case AnnotationType.Circle:
+                          // Logic for circle annotation
+                          break;
+                        case AnnotationType.Highlight:
+                          // Logic for highlight annotation
+                          break;
+                        default:
+                          // Handle other types or throw an error
+                          throw ArgumentError('Unknown annotation type: $type');
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
-          GestureDetector(
-            onTapUp: (details) {
-              _showAnnotationDialog(context, details.localPosition);
-            },
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Placeholder for button action if needed
-        },
-        child: Icon(Icons.edit),
-      ),
     );
   }
 
-  void _showAnnotationDialog(BuildContext context, Offset position) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Ajouter une annotation'),
-          content:
-              Text('Voulez-vous ajouter une annotation à cette position ?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Annuler'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _annoterPDF(context, position);
-              },
-              child: Text('Ajouter'),
-            ),
-          ],
+  void _addRectangleAnnotation() {
+    if (_pdfDocument != null && _pdfViewerController.pageNumber != null) {
+      final int pageNumber = _pdfViewerController.pageNumber!;
+      final int totalPages = _pdfDocument!.pages.count;
+
+      if (pageNumber >= 0 && pageNumber < totalPages) {
+        final PdfPage page = _pdfDocument!.pages[pageNumber];
+        final PdfRectangleAnnotation rectangleAnnotation =
+            PdfRectangleAnnotation(
+          Rect.fromLTWH(0, 30, 80, 80),
+          'Rectangle Annotation',
+          author: 'Syncfusion',
+          color: PdfColor(255, 0, 0),
+          setAppearance: true,
+          modifiedDate: DateTime.now(),
         );
-      },
-    );
-  }
 
-  Future<void> _annoterPDF(BuildContext context, Offset position) async {
-    try {
-      PdfDocument document =
-          PdfDocument(inputBytes: widget.pdfFile.readAsBytesSync());
-      PdfPage page = document.pages[_currentPage];
+        // Add the annotation to the PDF page
+        page.annotations.add(rectangleAnnotation);
 
-      double x = position.dx;
-      double y = page.size.height - position.dy;
-
-      PdfRectangleAnnotation rectangleAnnotation = PdfRectangleAnnotation(
-        Rect.fromLTWH(x, y, 80, 80),
-        'Rectangle Annotation',
-        author: 'Syncfusion',
-        color: PdfColor(255, 0, 0),
-        border: PdfAnnotationBorder(5),
-        modifiedDate: DateTime.now(),
-      );
-
-      page.annotations.add(rectangleAnnotation);
-
-      await widget.pdfFile.writeAsBytes(await document.save());
-      document.dispose();
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Annotation ajoutée au PDF')),
-      );
-
-      // Rafraîchir la vue PDF après l'ajout d'annotation
-      //_refreshPdfView();
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Erreur lors de l\'annotation du PDF: $e')),
-      );
+        // Save the document with annotations
+        _saveDocument(_pdfDocument!);
+      }
     }
   }
 
-  void _refreshPdfView() {
-    // Trigger a rebuild of the PDFView by changing the key
-    setState(() {
-      _tempFilePath = widget.pdfFile.path +
-          '?refresh=${DateTime.now().millisecondsSinceEpoch}';
-    });
+  Future<void> _saveDocument(PdfDocument document) async {
+    final List<int> bytes = await document.save();
+    // Do something with the bytes, for example, save to a file
+    // File('output.pdf').writeAsBytes(bytes);
   }
 }
---
+++ lib/components/tools_bar.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+enum AnnotationType {
+  Pen,
+  Text,
+  Rectangle,
+  Circle,
+  Highlight,
+}
+
+class ToolbarPDF extends StatefulWidget {
+  final Function(AnnotationType)? onAnnotationSelected;
+
+  const ToolbarPDF({
+    Key? key,
+    this.onAnnotationSelected,
+  }) : super(key: key);
+
+  @override
+  _ToolbarPDFState createState() => _ToolbarPDFState();
+}
+
+class _ToolbarPDFState extends State<ToolbarPDF> {
+  AnnotationType? _selectedType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 56.0,
+      color: Colors.blue.shade900,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _buildIconButton(AnnotationType.Pen, Icons.edit),
+          _buildIconButton(AnnotationType.Text, Icons.text_fields),
+          _buildIconButton(AnnotationType.Rectangle, Icons.crop_square),
+          _buildIconButton(AnnotationType.Circle, Icons.circle),
+          _buildIconButton(AnnotationType.Highlight, Icons.highlight),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIconButton(AnnotationType type, IconData iconData) {
+    final isSelected = _selectedType == type;
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          if (isSelected)
+            Container(
+              width: 36.0,
+              height: 36.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(255, 103, 179, 210),
+              ),
+            ),
+          Ink(
+            decoration: ShapeDecoration(
+              shape: CircleBorder(),
+              color: Colors.transparent,
+            ),
+            child: IconButton(
+              onPressed: () {
+                _selectAnnotation(type);
+              },
+              icon: Icon(iconData,
+                  color: isSelected
+                      ? Colors.yellow
+                      : Colors.white), // Jaune si sélectionné, sinon blanc
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _selectAnnotation(AnnotationType type) {
+    if (_selectedType == type) {
+      // Déselectionne si déjà sélectionné
+      setState(() {
+        _selectedType = null;
+      });
+    } else {
+      // Sélectionne sinon
+      setState(() {
+        _selectedType = type;
+      });
+    }
+
+    // Appelle la fonction de rappel avec le type sélectionné
+    if (widget.onAnnotationSelected != null) {
+      widget.onAnnotationSelected!(type);
+    }
+  }
+}
